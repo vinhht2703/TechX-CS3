73c6146aa93f3ceae566bd9e67b8c9dd42b9590e
Vinh Huynh <vinh.ht2703@gmail.com>
2024-05-27 19:03:45 +0700

    w6 - e3 - BST structure

---
+++ Week_6/ex3_BST/BST_structure.py
@@ -0,0 +1,13 @@
+{
+    "key": 13,
+    "left": {
+        "key": 4,
+        "left:": {"key": 2, "left": {}, "right": {}},
+        "right": {"key": 5, "left": {}, "right": {}},
+    },
+    "right": {
+        "key": 20,
+        "left": {"key": 14, "left": {}, "right": {}},
+        "right": {"key": 22, "left": {}, "right": {}},
+    },
+}
